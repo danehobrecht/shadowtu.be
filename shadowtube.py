59a12dafcda809a21f5e17fb21e7b5415bf3af75
Dane Hobrecht <70723095+danehobrecht@users.noreply.github.com>
2021-02-01 19:10:56 -0800

    Update shadowtube.py

--- shadowtube.py
+++ shadowtube.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
+# Dependencies: urllib3
 
 import urllib3
 import re
 
 ## Fetches and stores the inputted youtube URL's title
+
 url = raw_input("Enter YouTube URL: ")
 http = urllib3.PoolManager()
 fetch = http.request('GET', url)
@@ -11,4 +13,8 @@ s = fetch.data
 start = '{"title":{"runs":[{"text":"'
 end = '"}]},"viewCount"'
 title = s[s.find(start)+len(start):s.rfind(end)]
+
+## Formats link into YouTube title search
+
+title = "https://www.youtube.com/results?search_query=" + "+".join( title.split() )
 print(title)
