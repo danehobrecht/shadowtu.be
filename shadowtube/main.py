1f0ea658126547eb6b49a5eb8a7f8fe309229766
Dane Hobrecht <70723095+danehobrecht@users.noreply.github.com>
2021-02-05 10:18:56 -0800

    Add files via upload

---
+++ shadowtube/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# Dependencies: python-pip, python-pip3, urllib3
+# Example test url: https://youtu.be/Y6ljFaKRTrI
+
+import subprocess
+
+subprocess.call("./scrape.py", shell = False)
