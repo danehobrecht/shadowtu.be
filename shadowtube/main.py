7e8d15f2ac9a7b80910fd30abf3378211c77b449
Dane Hobrecht <70723095+danehobrecht@users.noreply.github.com>
2021-02-05 18:59:35 -0800

    Update main.py

--- shadowtube/main.py
+++ shadowtube/main.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python
 
 # Dependencies: sudo apt install python-pip, sudo apt install python-pip3, pip3 install urllib3, pip install PySocks, sudo apt install tor
-# Tor browser must be running for this script to execute successfully.
 # Example test url: https://youtu.be/Y6ljFaKRTrI
+# Tor browser must be running for this script to execute successfully.
 
 import urllib
 import urllib3
 import re
 import socks
 import socket
+import requests
+import time
+from stem.control import Controller
+from stem import Signal
+
+def get_tor_session():
+    # initialize a requests Session
+    session = requests.Session()
+    # setting the proxy of both http & https to the localhost:9050 
+    # this requires a running Tor service in your machine and listening on port 9050 (by default)
+    session.proxies = {"http": "socks5://localhost:9150", "https": "socks5://localhost:9150"}
+    return session
+
+def renew_connection():
+    with Controller.from_port(port = 9151) as c:
+        c.authenticate()
+        # send NEWNYM signal to establish a new clean connection through the Tor network
+        c.signal(Signal.NEWNYM)
+
+s = get_tor_session()
+ip = s.get("http://icanhazip.com").text
+print("\nCurrent IP: " + ip)
 
 videosAccessible = 0
-videosNonAccessible = 0
 attemptedRoutes = 0
-
-def create_connection(address, timeout = None, source_address = None):
-	sock = socks.socksocket()
-	sock.connect(address)
-	return sock
 
 def userInput():
 	global urlInput
@@ -45,7 +61,6 @@ def getTitle():
 
 def searchTitle():
 	global videosAccessible
-	global videosNonAccessible
 	global attemptedRoutes
 	print("Searching for instance...")
 	formatQuery = "https://www.youtube.com/results?search_query=" + "+".join(title.split())
@@ -57,17 +72,22 @@ def searchTitle():
 		attemptedRoutes += 1
 	else:
 		print("Not found!")
-		videosNonAccessible += 1
+		videosAccessible -= 1
+		if videosAccessible < 0:
+			videosAccessible = 0
 		attemptedRoutes += 1
 
 def execute():
 	userInput()
-	socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
-	socket.socket = socks.socksocket
-	socket.create_connection = create_connection
 	getTitle()
 	for x in range(0, 3, 1):
 		searchTitle()
-	print("Videos publicly accessible: " + str(videosAccessible) + "/" + str(attemptedRoutes))
+		print("Rotating IP...")
+		time.sleep(9)
+		renew_connection()
+		s = get_tor_session()
+		ip = s.get("http://icanhazip.com").text
+		print("\nIP used: " + ip)
+	print("Videos publicly accessible: " + str(videosAccessible) + "/" + str(attemptedRoutes) + "attempted routes.")
 
 execute()
