f984136b994787a91b11269213029f26ae7c59c4
Dane Hobrecht <70723095+danehobrecht@users.noreply.github.com>
2021-02-16 16:18:33 -0800

    Update main.py

--- shadowtube/main.py
+++ shadowtube/main.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # Example test url: https://youtu.be/Y6ljFaKRTrI
 
+from __future__ import print_function
+from lxml.cssselect import CSSSelector
 from stem.control import Controller
 from stem import Signal
 
@@ -14,6 +16,10 @@ import re
 import requests
 import time
 import io
+import json
+import os
+import argparse
+import lxml.html
 
 videosAccessible = 0
 commentsAccessible = 0
@@ -127,21 +133,24 @@ def getComments(): #https://www.youtube.com/feed/history/comment_history
 
 def searchComment():
 	global commentsAccessible, attemptedRoutesC
-	print("\nSearching for comment...\n\n"),
 	i = 0;
 	for i in range(links.count("'") / 2):
 		i += 1
-		link = links.split("'")[i]
+		time.sleep(3)
+		youtube_id = links.split("'")[i]
 		comment = commentIds.split("'")[i]
+		fetchComments(youtube_id.replace('https://www.youtube.com/watch?v=', ''))
+		print("Link being analyzed: " + youtube_id)
+		print("CID in question: " + comment + "\n")
+		print("Searching for comment..."),
 		with open('json.json', 'r') as json:
     			b = json.read()
-		#print(link + " " + comment)
 		if b.find(comment) >= 0:
-			print("Comment found!\n")
+			print("Comment found!")
 			commentsAccessible += 1
 			attemptedRoutesC += 1
 		else:
-			print("Comment not found.\n")
+			print("Comment not found.")
 			commentsAccessible -= 1
 			if commentsAccessible < 0:
 				commentsAccessible = 0
@@ -157,6 +166,111 @@ def commentsExecute():
 		time.sleep(9)
 		renewConnection()
 	print(str(commentsAccessible) + "/" + str(attemptedRoutesC) + " public comments found.\n")
+
+# Downloader
+
+YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v={youtube_id}'
+YOUTUBE_COMMENTS_AJAX_URL = 'https://www.youtube.com/comment_service_ajax'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+
+def find_value(html, key, num_chars = 2, separator = '"'):
+    pos_begin = html.find(key) + len(key) + num_chars
+    pos_end = html.find(separator, pos_begin)
+    return html[pos_begin: pos_end]
+
+def ajax_request(session, url, params = None, data = None, headers = None, retries = 5, sleep = 20):
+    for _ in range(retries):
+        response = session.post(url, params=params, data=data, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        if response.status_code in [403, 413]:
+            return {}
+        else:
+            time.sleep(sleep)
+
+def download_comments(youtube_id, sleep = .1):
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+
+    response = session.get(YOUTUBE_VIDEO_URL.format(youtube_id = youtube_id))
+    html = response.text
+    session_token = find_value(html, 'XSRF_TOKEN', 3)
+    session_token = session_token.encode('ascii').decode('unicode-escape')
+
+    data = json.loads(find_value(html, 'var ytInitialData = ', 0, '};') + '}')
+    for renderer in search_dict(data, 'itemSectionRenderer'):
+        ncd = next(search_dict(renderer, 'nextContinuationData'), None)
+        if ncd:
+            break
+    continuations = [(ncd['continuation'], ncd['clickTrackingParams'])]
+
+    while continuations:
+        continuation, itct = continuations.pop()
+        response = ajax_request(session, YOUTUBE_COMMENTS_AJAX_URL,
+                                params={'action_get_comments': 1,
+                                        'pbj': 1,
+                                        'ctoken': continuation,
+                                        'continuation': continuation,
+                                        'itct': itct},
+                                data={'session_token': session_token},
+                                headers={'X-YouTube-Client-Name': '1',
+                                         'X-YouTube-Client-Version': '2.20201202.06.01'})
+
+        if not response:
+            break
+        if list(search_dict(response, 'externalErrorMessage')):
+            raise RuntimeError('Error returned from server: ' + next(search_dict(response, 'externalErrorMessage')))
+
+        continuations = [(ncd['continuation'], ncd['clickTrackingParams'])
+                         for ncd in search_dict(response, 'nextContinuationData')] + continuations
+
+        for comment in search_dict(response, 'commentRenderer'): # downloads comments
+            yield {'cid': comment['commentId'],'text': ''.join([c['text'] for c in comment['contentText']['runs']])}
+
+        time.sleep(sleep)
+
+def search_dict(partial, search_key):
+    stack = [partial]
+    while stack:
+        current_item = stack.pop()
+        if isinstance(current_item, dict):
+            for key, value in current_item.items():
+                if key == search_key:
+                    yield value
+                else:
+                    stack.append(value)
+        elif isinstance(current_item, list):
+            for value in current_item:
+                stack.append(value)
+
+
+def fetchComments(youtube_id):
+    parser = argparse.ArgumentParser()
+    try:	
+        args = parser.parse_args()
+        output = 'json.json'
+        limit = 100
+        if not youtube_id or not output:
+            parser.print_usage()
+            raise ValueError('Faulty YouTube Video ID.')
+        if os.sep in output:
+            outdir = os.path.dirname(output)
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+	print('\nDownloading comments (depending on the video, this may take a while)...')
+        print('Video ID: ' + youtube_id + "\n")
+        count = 0
+        with io.open(output, 'w', encoding = 'utf8') as fp:
+            for comment in download_comments(youtube_id):
+                comment_json = json.dumps(comment, ensure_ascii=False)
+                print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
+                count += 1
+                if limit and count >= limit:
+                    break
+        print('Done!\n')
+    except Exception as e:
+        print('Error:', str(e))
+        exit()
 
 # Menu
 
